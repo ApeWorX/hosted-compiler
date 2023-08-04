9ed782f98b591fa83cb4510098e381e3247136af
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-08-04 15:16:04 -0700

    feat: get request is the issue with localhost docs

--- main.py
+++ main.py
@@ -30,16 +30,19 @@ def is_supported_language(filename):
     _, file_extension = os.path.splitext(filename)
     return file_extension.lower() in supported_languages
 
-async def compile_files(task_id, files): #vyper_version):
+async def compile_files(task_id, files, vyper_version):
     # Your Vyper compilation logic here
     # This is just a dummy example that prints the filenames and Vyper version
     tasks[task_id] = Task(id=task_id)
     print(f"Task ID: {task_id}")
+
+# TODO Make this do something with ape to compile the files into a manifest
+
     await asyncio.sleep(60)
     print("Files to compile:")
     for file in files:
         print(file)
-    #print(f"Vyper version: {vyper_version}")
+    print(f"Vyper version: {vyper_version}")
     tasks[task_id].status = random.choice([TaskStatus.FAILED, TaskStatus.SUCCESS])
 
     if tasks[task_id].status is TaskStatus.FAILED:
@@ -54,7 +57,7 @@ async def compile_files(task_id, files): #vyper_version):
 async def create_compilation_task(
     background_tasks: BackgroundTasks,
     files: List[UploadFile],
-    # vyper_version: str = Query(..., title="Vyper version to use for compilation"),
+    vyper_version: str = Query(..., title="Vyper version to use for compilation"),
 ):
     supported_files = [file.filename for file in files if is_supported_language(file.filename)]
     if not supported_files:
@@ -64,7 +67,7 @@ async def create_compilation_task(
     task_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
     # Run the compilation task in the background using TaskIQ
-    background_tasks.add_task(compile_files, task_id, supported_files) #vyper_version)
+    background_tasks.add_task(compile_files, task_id, supported_files, vyper_version)
     return {"task_id": task_id}
 
 
@@ -87,12 +90,13 @@ async def get_task_exceptions(task_id: str) -> List[str]:
         raise HTTPException(status_code=400, detail="Task is not completed with Error status")
     return tasks[task_id].exceptions
 
-@app.get("/compiled_artifact/{task_id}")
-async def get_compiled_artifact(task_id: str) -> PackageManifest:
+@app.get("/compiled_artifact/{task_id}", response_model = PackageManifest)
+async def get_compiled_artifact(task_id: str):
     # Fetch the compiled artifact data in ethPM v3 format for a particular task
     # This is just a dummy example, you should handle the response data accordingly based on the actual compilation result
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
-    if tasks[id].status is not TaskStatus.SUCCESS:
-        raise HTTPException(status_code=400, detail="Task is not completed with Success status")
+    if tasks[task_id].status is not TaskStatus.SUCCESS:
+        raise HTTPException(status_code=404, detail="Task is not completed with Success status")
+    
     return tasks[task_id].manifest
