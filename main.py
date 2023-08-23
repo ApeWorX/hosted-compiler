adfe303bc9b658349d916b36f8bfed40dc176595
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-08-22 17:55:46 -0700

    chore: black cleaning

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from ape import compilers, config
 from pathlib import Path
 
 
-
 PackageManifest.update_forward_refs()
 app = FastAPI()
 
@@ -45,6 +44,7 @@ def is_supported_language(filename):
     supported_languages = [".vy"]
     _, file_extension = os.path.splitext(filename)
     return file_extension.lower() in supported_languages
+
 
 @app.post("/compile/")
 async def create_compilation_task(
@@ -82,6 +82,7 @@ async def get_task_status(task_id: str) -> TaskStatus:
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     return tasks[task_id]
+
 
 @app.get("/exceptions/{task_id}")
 async def get_task_exceptions(task_id: str) -> List[str]:
