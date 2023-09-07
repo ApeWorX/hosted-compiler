0846b2030b8471336454850ca7190bd8345a6521
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-09-07 14:41:49 -0700

    feat: added comment and a better readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-chris
+# Overview
+
+**Vyper Compiler** is a tool used to help [Remix Online IDE](https://remix-project.org/) compile Vyper contracts.
+
+Vyper Compiler is built by [ApeWorX LTD](https://www.apeworx.io/).
+
+If you have any questions please ask in our discord [ApeWorX Discord server](https://discord.gg/apeworx).
+
+## Documentation
+
+The compiler is built with Ape and fastAPI. It has multiple routes to engage the compiler in different parts of your journey to  compiler the contract.
+
+
+## Quickstart with local contracts
+
+To show that it works. Clone this repo and deploy the local host server with `uvicorn main:app --reload`
+
+Using the post route: Upload the contract file and version number of vyper.
+
+Wait for it compile and while you wait, you can use the temporary directory name is the task ID to check the status of it.
+
+Once it says success, you can use the `get compiled artifact` route and return the manifest of the contract.
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, BackgroundTasks, Query
-import asyncio
 import os
-import random
-import string
 from pydantic import BaseModel
 from enum import Enum
 from typing import Dict, Optional, List
 from ethpm_types import PackageManifest
 
 import tempfile
-import vyper
-import ipdb
 
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 from ape import compilers, config
 from pathlib import Path
+
+
 
 
 PackageManifest.update_forward_refs()
@@ -42,6 +39,9 @@ tasks: Dict[str, TaskStatus] = {}
 
 
 def is_supported_language(filename):
+    """
+    Checks if the file is a vyper file.
+    """
     # Add your supported languages here (Vyper)
     supported_languages = [".vy"]
     _, file_extension = os.path.splitext(filename)
@@ -54,6 +54,10 @@ async def create_compilation_task(
     files: List[UploadFile],
     vyper_version: str = Query(..., title="Vyper version to use for compilation"),
 ):
+    """
+    Creates the task with the list of vyper contracts to compile 
+    and sets each file with a task.
+    """
     project_root = Path(tempfile.mkdtemp(""))
 
     # Create a contracts directory
@@ -74,9 +78,9 @@ async def create_compilation_task(
 
 @app.get("/status/{task_id}")
 async def get_task_status(task_id: str) -> TaskStatus:
-    # Check the status of the task (dummy example, replace with actual status tracking)
-    # You should maintain the task status in a database or some other persistent storage
-    # Here, we just return random statuses for illustration purposes
+    """
+    Check the status of each task
+    """
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     return tasks[task_id]
@@ -84,8 +88,9 @@ async def get_task_status(task_id: str) -> TaskStatus:
 
 @app.get("/exceptions/{task_id}")
 async def get_task_exceptions(task_id: str) -> List[str]:
-    # Fetch the exception information for a particular compilation task
-    # This is just a dummy example, you should handle exceptions and errors appropriately
+    """
+    Fetch the exception information for a particular compilation task
+    """
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     if tasks[id].status is not TaskStatus.FAILED:
@@ -97,8 +102,9 @@ async def get_task_exceptions(task_id: str) -> List[str]:
 
 @app.get("/compiled_artifact/{task_id}")
 async def get_compiled_artifact(task_id: str) -> Dict:
-    # Fetch the compiled artifact data in ethPM v3 format for a particular task
-    # This is just a dummy example, you should handle the response data accordingly based on the actual compilation result
+    """
+    Fetch the compiled artifact data in ethPM v3 format for a particular task
+    """
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     if tasks[task_id] is not TaskStatus.SUCCESS:
@@ -110,7 +116,9 @@ async def get_compiled_artifact(task_id: str) -> Dict:
 
 
 async def compile_project(project_root: Path, files: List[UploadFile]):
-    # compile project
+    """
+    Compile the contrct and asssign the taskid to it
+    """
     with config.using_project(project_root) as project:
         results[project_root.name] = project.extract_manifest()
     tasks[project_root.name] = TaskStatus.SUCCESS
