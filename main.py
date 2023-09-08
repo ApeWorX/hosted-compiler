79011733153c67020bcdc4d0435c70f1af4be955
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-09-08 11:39:00 -0700

    fix: merge conflicts

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ from ethpm_types import PackageManifest
 
 import tempfile
 
+<<<<<<< HEAD
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
+=======
+>>>>>>> bb1f7ac... feat: got a vyper compile to work
 from ape import compilers, config
 from pathlib import Path
 
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> bb1f7ac... feat: got a vyper compile to work
 PackageManifest.update_forward_refs()
 app = FastAPI()
 
@@ -37,6 +43,9 @@ class Task(BaseModel):
 results = {}
 tasks: Dict[str, TaskStatus] = {}
 
+#global db
+results = {}
+tasks: Dict[str,TaskStatus] = {}
 
 def is_supported_language(filename):
     """
@@ -47,27 +56,14 @@ def is_supported_language(filename):
     _, file_extension = os.path.splitext(filename)
     return file_extension.lower() in supported_languages
 
-
 @app.post("/compile/")
 async def create_compilation_task(
     background_tasks: BackgroundTasks,
     files: List[UploadFile],
     vyper_version: str = Query(..., title="Vyper version to use for compilation"),
-):
-    """
-    Creates the task with the list of vyper contracts to compile 
-    and sets each file with a task.
-    """
+):  
+    content = await files[0].read()
     project_root = Path(tempfile.mkdtemp(""))
-
-    # Create a contracts directory
-    contracts_dir = project_root / "contracts"
-    contracts_dir.mkdir()
-
-    # add request contracts in temp directory
-    for file in files:
-        content = (await file.read()).decode("utf-8")
-        (project_root / "contracts" / file.filename).write_text(content)
 
     tasks[project_root.name] = TaskStatus.PENDING
     # Run the compilation task in the background using TaskIQ
@@ -85,7 +81,6 @@ async def get_task_status(task_id: str) -> TaskStatus:
         raise HTTPException(status_code=404, detail="task id not found")
     return tasks[task_id]
 
-
 @app.get("/exceptions/{task_id}")
 async def get_task_exceptions(task_id: str) -> List[str]:
     """
@@ -94,11 +89,8 @@ async def get_task_exceptions(task_id: str) -> List[str]:
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     if tasks[id].status is not TaskStatus.FAILED:
-        raise HTTPException(
-            status_code=400, detail="Task is not completed with Error status"
-        )
+        raise HTTPException(status_code=400, detail="Task is not completed with Error status")
     return tasks[task_id]
-
 
 @app.get("/compiled_artifact/{task_id}")
 async def get_compiled_artifact(task_id: str) -> Dict:
@@ -108,17 +100,29 @@ async def get_compiled_artifact(task_id: str) -> Dict:
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
     if tasks[task_id] is not TaskStatus.SUCCESS:
-        raise HTTPException(
-            status_code=404, detail="Task is not completed with Success status"
-        )
-    # TODO Debug why it is producing serialize_response raise ResponseValidationError( fastapi.exceptions.ResponseValidationError ) when you use return results[task_id] as a PackageManifest
-    return results[task_id].dict()
+        raise HTTPException(status_code=404, detail="Task is not completed with Success status")
+    
+    return results[task_id]
+
 
 
 async def compile_project(project_root: Path, files: List[UploadFile]):
-    """
-    Compile the contrct and asssign the taskid to it
-    """
+
+# Create a contracts directory
+    contracts_dir = project_root / "contracts"
+    contracts_dir.mkdir()
+
+# add request contracts in temp directory
+# files[0].headers["content-disposition"].split("; ")[-1].split("=")[-1].strip('"')
+    for file in files:
+        filename = file.headers["content-disposition"].split("; ")[-1].split("=")[-1].strip('"')
+        content = await file.read()
+        (project_root / 'contracts'/ filename).write_text(content.decode("utf-8"))
+        
+#compile project
     with config.using_project(project_root) as project:
         results[project_root.name] = project.extract_manifest()
+    
     tasks[project_root.name] = TaskStatus.SUCCESS
+
+    
