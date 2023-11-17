01f8f440314dbd110ecc6406481ec3cca55107e6
Chris <15219900+Ninjagod1251@users.noreply.github.com>
2023-11-17 10:16:06 +0300

    feat: added alternative file transfer (#24)
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- main.py
+++ main.py
@@ -15,8 +15,50 @@ from ape import compilers, config
 from pathlib import Path
 
 
+from fastapi import FastAPI, Request
+from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
+from fastapi.responses import HTMLResponse
+
+
+def init_openapi(app: FastAPI):
+    # https://github.com/tiangolo/fastapi/discussions/10524
+    # Copied from FastAPI, and customized the version
+    async def swagger_ui_html(req: Request) -> HTMLResponse:
+        root_path = req.scope.get("root_path", "").rstrip("/")
+        openapi_url = root_path + app.openapi_url
+        oauth2_redirect_url = app.swagger_ui_oauth2_redirect_url
+        if oauth2_redirect_url:
+            oauth2_redirect_url = root_path + oauth2_redirect_url
+        return get_swagger_ui_html(
+            openapi_url=openapi_url,
+            title=app.title + " - Swagger UI",
+            oauth2_redirect_url=oauth2_redirect_url,
+            init_oauth=app.swagger_ui_init_oauth,
+            swagger_ui_parameters=app.swagger_ui_parameters,
+            swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
+            swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui.css",
+        )
+
+    app.add_route('/docs', swagger_ui_html, include_in_schema=False)
+
+    if app.swagger_ui_oauth2_redirect_url:
+
+        async def swagger_ui_redirect(req: Request) -> HTMLResponse:
+            return get_swagger_ui_oauth2_redirect_html()
+
+        app.add_route(
+            app.swagger_ui_oauth2_redirect_url,
+            swagger_ui_redirect,
+            include_in_schema=False,
+        )
+
+
+app = FastAPI(
+    docs_url=None,  # https://github.com/tiangolo/fastapi/discussions/10524
+)
+init_openapi(app)
+
 PackageManifest.update_forward_refs()
-app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -28,12 +70,12 @@ app.add_middleware(
         "https://remix.ethereum.org",
         "https://remix-alpha.ethereum.org",
         "https://remix-beta.ethereum.org",
+        "https://deploy-preview-*--remixproject.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 class TaskStatus(Enum):
     PENDING = "PENDING"
@@ -63,11 +105,10 @@ def is_supported_language(filename):
     return file_extension.lower() in supported_languages
 
 
-@app.post("/compile/")
+@app.post("/compile")
 async def create_compilation_task(
     background_tasks: BackgroundTasks,
-    files: List[UploadFile],
-    vyper_version: str = Query(..., title="Vyper version to use for compilation"),
+    manifest: PackageManifest,
 ):
     """
     Creates the task with the list of vyper contracts to compile
@@ -75,18 +116,9 @@ async def create_compilation_task(
     """
     project_root = Path(tempfile.mkdtemp(""))
 
-    # Create a contracts directory
-    contracts_dir = project_root / "contracts"
-    contracts_dir.mkdir()
-
-    # add request contracts in temp directory
-    for file in files:
-        content = (await file.read()).decode("utf-8")
-        (project_root / "contracts" / file.filename).write_text(content)
-
     tasks[project_root.name] = TaskStatus.PENDING
     # Run the compilation task in the background using TaskIQ
-    background_tasks.add_task(compile_project, project_root, files)
+    background_tasks.add_task(compile_project, project_root, manifest)
 
     return project_root.name
 
@@ -108,7 +140,7 @@ async def get_task_exceptions(task_id: str) -> List[str]:
     """
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="task id not found")
-    if tasks[id].status is not TaskStatus.FAILED:
+    if tasks[task_id] is not TaskStatus.FAILED:
         raise HTTPException(
             status_code=400, detail="Task is not completed with Error status"
         )
@@ -116,7 +148,7 @@ async def get_task_exceptions(task_id: str) -> List[str]:
 
 
 @app.get("/compiled_artifact/{task_id}")
-async def get_compiled_artifact(task_id: str) -> Dict:
+async def get_compiled_artifact(task_id: str) -> PackageManifest:
     """
     Fetch the compiled artifact data in ethPM v3 format for a particular task
     """
@@ -127,13 +159,32 @@ async def get_compiled_artifact(task_id: str) -> Dict:
             status_code=404, detail="Task is not completed with Success status"
         )
     # TODO Debug why it is producing serialize_response raise ResponseValidationError( fastapi.exceptions.ResponseValidationError ) when you use return results[task_id] as a PackageManifest
-    return results[task_id].dict()
+    return results[task_id]
 
 
-async def compile_project(project_root: Path, files: List[UploadFile]):
+async def compile_project(project_root: Path, manifest: PackageManifest):
     """
     Compile the contrct and asssign the taskid to it
     """
+    # Create a contracts directory
+    contracts_dir = project_root / "contracts"
+    contracts_dir.mkdir()
+
+    # add request contracts in temp directory
+    if manifest.sources:
+        for filename, source in manifest.sources.items():
+            path = (contracts_dir / filename)
+            # NOTE: In case there is a multi-level path
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.write_text(source.fetch_content())
+
+    (project_root / ".build").mkdir()
+    (project_root / ".build" / "__local__.json").write_text(manifest.json())
+
     with config.using_project(project_root) as project:
-        results[project_root.name] = project.extract_manifest()
-    tasks[project_root.name] = TaskStatus.SUCCESS
+         try:
+             results[project_root.name] = project.extract_manifest()
+             tasks[project_root.name] = TaskStatus.SUCCESS
+         except Exception as e:
+             results[project_root.name] = [str(e)]
+             tasks[project_root.name] = TaskStatus.FAILED
