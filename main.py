dae25046c4ed871162762389ef027af79fa14acd
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-11-18 17:14:17 +0300

    fix: uniform error

--- main.py
+++ main.py
@@ -115,12 +115,12 @@ async def create_compilation_task(
     and sets each file with a task.
     """
     project_root = Path(tempfile.mkdtemp(""))
-
-    tasks[project_root.name] = TaskStatus.PENDING
+    task_id = project_root.name
+    tasks[task_id] = TaskStatus.PENDING
     # Run the compilation task in the background using TaskIQ
     background_tasks.add_task(compile_project, project_root, manifest)
 
-    return project_root.name
+    return task_id
 
 
 @app.get("/status/{task_id}")
@@ -129,7 +129,7 @@ async def get_task_status(task_id: str) -> TaskStatus:
     Check the status of each task
     """
     if task_id not in tasks:
-        raise HTTPException(status_code=404, detail="task id not found")
+        raise HTTPException(status_code=404, detail=f"Task: {task_id} not found")
     return tasks[task_id]
 
 
@@ -142,7 +142,7 @@ async def get_task_exceptions(task_id: str) -> List[str]:
         raise HTTPException(status_code=404, detail="task id not found")
     if tasks[task_id] is not TaskStatus.FAILED:
         raise HTTPException(
-            status_code=400, detail="Task is not completed with Error status"
+            status_code=400, detail=f"Task: {task_id} is not completed with Error status"
         )
     return tasks[task_id]
 
@@ -153,10 +153,10 @@ async def get_compiled_artifact(task_id: str) -> Any:
     Fetch the compiled artifact data in ethPM v3 format for a particular task
     """
     if task_id not in tasks:
-        raise HTTPException(status_code=404, detail=f"{task_id} not found")
+        raise HTTPException(status_code=404, detail=f"Task: {task_id} not found")
     if tasks[task_id] is not TaskStatus.SUCCESS:
         raise HTTPException(
-            status_code=404, detail=f"{task_id} is not completed with Success status"
+            status_code=404, detail=f"Task: {task_id} is not completed with Success status"
         )
     # TODO Debug why it is producing serialize_response raise ResponseValidationError( fastapi.exceptions.ResponseValidationError ) when you use return results[task_id] as a PackageManifest
     return results[task_id]
