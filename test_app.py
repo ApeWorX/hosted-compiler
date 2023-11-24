4c252b4e41e7718c76cf1b15644a11b48d93caba
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2023-11-24 15:33:43 -0500

    refactor: use poetry (#28)

--- test_app.py
+++ test_app.py
@@ -7,19 +7,13 @@ client = TestClient(app)
 
 def test_create_compilation_task():
     # Test create compilation task
-    response = client.post("/compile/")
+    response = client.post("/compile")
     assert (
         response.status_code == 422
-    )  # Since the files parameter is missing, it should return 422 Unprocessable Entity
+    )  # Since the manifest parameter is missing, it should return 422 Unprocessable Entity
 
-    # Upload a valid Vyper file
-    files = {
-        "files": (
-            "my_contract.vy",
-            open("path_to_your_vyper_file/my_contract.vy", "rb"),
-        )
-    }
-    response = client.post("/compile/", files=files, data={"vyper_version": "0.2.10"})
+    # empty sources should work
+    response = client.post("/compile", json={"manifest": "ethpm/3"})
     assert response.status_code == 200
     data = response.json()
     assert "task_id" in data
@@ -51,11 +45,11 @@ def test_get_task_exceptions():
 
 def test_get_compiled_artifact():
     # Test get compiled artifact
-    response = client.get("/compiled_artifact/some_invalid_task_id")
+    response = client.get("/artifacts/some_invalid_task_id")
     assert response.status_code == 400  # Invalid task_id should return 400 Bad Request
 
     # Assuming the task_id has a Success status
-    response = client.get("/compiled_artifact/some_valid_task_id")
+    response = client.get("/artifacts/some_valid_task_id")
     assert response.status_code == 200
     data = response.json()
     assert "contract_name" in data
