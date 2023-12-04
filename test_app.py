d9350ae7083ac1ca5b319396e9b5d5e5edfcbdfa
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2023-12-04 10:52:57 -0800

    fix: added access control

--- test_app.py
+++ test_app.py
@@ -57,9 +57,7 @@ def test_get_compiled_artifact():
     assert "compiler" in data
 
 def test_cors_allowed_origins():
-    # Define a list of allowed origins based on the configuration in main.py
-    env_allowed_origin = os.environ.get("VYPER_HOSTED_COMPILER_CORS_ALLOWED_ORIGINS")
-    
+    # Define a list of allowed origins based on the configuration in main.py    
     allowed_origins = [
         "http://localhost:8080",
         "http://localhost:4173",
@@ -67,23 +65,25 @@ def test_cors_allowed_origins():
         "https://remix-alpha.ethereum.org",
         "https://remix-beta.ethereum.org",
         "https://deploy-preview-123--remixproject.netlify.app",
-        "https://deploy-preview-3333--remixproject.netlify.app",
-    ]
+        "https://deploy-preview-3333--remixproject.netlify.app",        
+        os.environ["VYPER_HOSTED_COMPILER_CORS_ALLOWED_ORIGINS"] = ["test-from-env-var"],
 
-    if env_allowed_origin:
-        allowed_origins.append(env_allowed_origin)
+    ]
 
     for origin in allowed_origins:
         response = client.options("/docs", headers={"Origin": origin})
         assert response.status_code == 200, f"Failed for origin: {origin}"
 
+        # Check if the response contains the appropriate CORS headers
+        assert response.headers.get("access-control-allow-origin") == origin, f"Failed for origin: {origin}"
 
     # Test an invalid origin and make sure it is not allowed
     invalid_origin = "https://invalid-origin.com"
     response = client.options("/docs", headers={"Origin": invalid_origin})
-
-    # Check if the response status code is not 200 (OK), indicating the origin is not allowed
     assert response.status_code != 200, "Invalid origin should not be allowed"
+
+    # Check if the response does not contain the CORS headers for the invalid origin
+    assert "access-control-allow-origin" not in response.headers, "Invalid origin should not be in CORS headers"
 
 
 
